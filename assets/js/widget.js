2e05b8500303d7f37a8c90c774e35696334d4be7
veks <veeeeeeeeek@gmail.com>
2017-09-26 19:07:03 +0300

    remove all files

--- assets/js/widget.js
+++
@@ -1,9 +0,0 @@
-/*!
- * Button visually impaired v1.0.5
- */
-jQuery(document).ready(function(){
-    jQuery('#widgets-right .color-picker, .inactive-sidebar .color-picker').wpColorPicker();
-});
-jQuery(document).ajaxComplete(function() {
-    jQuery('#widgets-right .color-picker, .inactive-sidebar .color-picker').wpColorPicker();
-});
--- uninstall.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('WP_UNINSTALL_PLUGIN'))
-	exit();
-
-$option_name = 'database_bvi';
-delete_option( $option_name );
